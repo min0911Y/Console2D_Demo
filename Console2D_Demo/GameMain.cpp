6cf4838adbcce2abb711d08f980a98e8ff25e97a
0911 min <1474635462@qq.com>
2022-02-07 20:30:11 +0800

    init

--- Console2D_Demo/GameMain.cpp
+++ Console2D_Demo/GameMain.cpp
@@ -1,6 +1,10 @@
 #include "GameMain.h"
 void Console2D::start()
 {
+	this->initsprite("X"); //设置主角样貌
+	this->initspritexy(10, 14); //设置主角初始位置
+	this->initWorld(20, 40);//设置世界大小
+	Other::HideCursor(); //隐藏光标
 }
 void Console2D::update()
 {
