db5e80406afda123be9086e158194dd6e3cc8a0a
0911 min <1474635462@qq.com>
2022-02-07 20:57:15 +0800

    画图函数声明

---
+++ Console2D_Demo/draw_function.h
@@ -0,0 +1,13 @@
+#ifndef DRAE_H
+#define DRAW_H
+#include "CONSOLE2D.h"
+namespace Map
+{
+	namespace Draw
+	{
+		namespace Welcome_ui {
+			void MAIN(Console2D *console);
+		};
+	};
+};
+#endif
---
+++ Console2D_Demo/map.cpp
@@ -0,0 +1,5 @@
+#include "draw_function.h"
+void Map::Draw::Welcome_ui::MAIN(Console2D *console)
+{
+
+}
